684f80683ebc04fde7b67225760163d5ade505c6
codebysana <sana.younas0530@gmail.com>
2026-07-06 23:56:03 +0500

    Add TypeScript interfaces for Course, User, and related entities

---
+++ client/app/types/course.tsx
@@ -0,0 +1,104 @@
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  avatar?: {
+    url: string;
+  };
+}
+
+export interface Avatar {
+  url: string;
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  avatar?: Avatar;
+  allCourses?: {
+    _id: string;
+  }[];
+}
+
+
+export interface Link {
+  title: string;
+  url: string;
+}
+
+export interface CourseContentResponse {
+  success: boolean;
+  content: CourseContent[];
+}
+
+export interface QuestionReply {
+  _id: string;
+  user: User;
+  answer: string;
+  createdAt?: string;
+}
+
+export interface Question {
+  _id: string;
+  question: string;
+  createdAt: string;
+  user: User;
+  questionReplies: QuestionReply[];
+}
+
+export interface CourseContent {
+  _id: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  links: Link[];
+  questions: Question[];
+  tags?: string;
+  videoSection: string;
+  videoDuration: number;
+}
+
+export interface ReviewReply {
+  _id: string;
+  comment: string;
+  createdAt: string;
+  user: User;
+}
+
+export interface Review {
+  _id: string;
+  rating: number;
+  comment: string;
+  createdAt: string;
+  user: User;
+  commentReplies: ReviewReply[];
+}
+
+export interface Benefit {
+  title: string;
+}
+
+export interface Prerequisite {
+  title: string;
+}
+
+export interface Course {
+  _id: string;
+  name: string;
+  description: string;
+  tags: string;
+  demoUrl: string;
+  ratings: number;
+  price: number;
+  estimatedPrice: number;
+  purchased: number;
+  benefits: Benefit[];
+  prerequisites: Prerequisite[];
+  courseData: CourseContent[];
+  reviews: Review[];
+}
+
+export interface CourseDetailsResponse {
+  course: Course;
+}
